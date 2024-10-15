00665e460bacdf0d2665e0566785056ce92a7e5e
Wen <wen880206@gmail.com>
2024-10-15 14:14:09 +0800

    feat: orderService UT完成

--- backend/tests/order.service.test.ts
+++ backend/tests/order.service.test.ts
@@ -1,13 +1,18 @@
 import orderService from '../src/services/order.service';
 import orderModel from '../src/models/order.model';
-
+import uploadService from '../src/services/upload.service';
+import { Order } from '../src/entities/order';
 jest.mock('../src/models/order.model', () => ({
     add: jest.fn(),
+    remove: jest.fn(),
+    update: jest.fn(),
+    findAll: jest.fn(),
+    saveAllOrders: jest.fn(),
 }));
 
 describe('Order Service', () => {
 
-    const mockOrderData = {
+    const mockOrderData1 = {
         id: 'af3a18b3-b703-4498-a451-cec5dc23b529',
         product_type: 'Am',
         product_name: '多維爾菊石',
@@ -18,16 +23,70 @@ describe('Order Service', () => {
         sale_date: new Date('2023-09-13')
     };
 
+    const mockOrderData2 = {
+        id: 'bf3a18b3-b703-4498-a451-cec5dc23b530',
+        product_type: 'Bm',
+        product_name: '化石',
+        sale_price: 2000,
+        buyer_name: 'Bob',
+        income: 2120,
+        receiver_name: '王小明',
+        sale_date: new Date('2023-09-14')
+    };
+
     beforeEach(() => {
         jest.clearAllMocks();
     });
 
     test('should add an order', async () => {
-         (orderModel.add as jest.Mock).mockResolvedValue(mockOrderData);
+         (orderModel.add as jest.Mock).mockResolvedValue(mockOrderData1);
 
-         const result = await orderService.add(mockOrderData);
+         const result = await orderService.add(mockOrderData1);
 
-         expect(orderModel.add).toHaveBeenCalledWith(mockOrderData);
-         expect(result).toEqual(mockOrderData);
+         expect(orderModel.add).toHaveBeenCalledWith(mockOrderData1);
+         expect(result).toEqual(mockOrderData1);
+    });
+
+    test('should remove an order', async () => {
+        (orderModel.remove as jest.Mock).mockResolvedValue(mockOrderData1);
+
+        const result = await orderService.deleteOrder({ id: mockOrderData1.id });
+
+        expect(orderModel.remove).toHaveBeenCalledWith(mockOrderData1.id);
+        expect(result).toEqual(mockOrderData1);
+    });
+
+    test('should update an order', async () => {
+        (orderModel.update as jest.Mock).mockResolvedValue(mockOrderData1);
+
+        const result = await orderService.update({ ...mockOrderData1 });
+
+        expect(orderModel.update).toHaveBeenCalledWith(mockOrderData1.id, mockOrderData1);
+        expect(result).toEqual(mockOrderData1);
+    });
+
+    test('should find all orders', async () => {
+        (orderModel.findAll as jest.Mock).mockResolvedValue([mockOrderData1]);
+
+        const expectedData = [{
+            ...mockOrderData1,
+            sale_price: Math.round(mockOrderData1.sale_price),
+            income: Math.round(mockOrderData1.income),
+            sale_date: mockOrderData1.sale_date.toISOString().split('T')[0]
+        }];
+
+        const result = await orderService.findAll();
+
+        expect(orderModel.findAll).toHaveBeenCalled();
+        expect(result).toEqual(expectedData);
+    });
+    
+    test('should save all orders', async () => {
+        (orderModel.saveAllOrders as jest.Mock).mockResolvedValue([mockOrderData1, mockOrderData2]);
+
+        const result = await uploadService.saveAllOrders([mockOrderData1, mockOrderData2] as Order[]);
+
+        expect(orderModel.saveAllOrders).toHaveBeenCalledWith([mockOrderData1, mockOrderData2]);
+        expect(result).toEqual([mockOrderData1, mockOrderData2]);
     });
 });
